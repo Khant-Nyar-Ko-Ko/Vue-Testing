58c4bf5403efb4e83d6fbd499ad01fcb3d22c3c2
Khant-Nyar-Ko-Ko <kokhantnyar19@gmail.com>
2023-11-12 23:34:36 +0700

    vue-test

---
+++ src/views/HomeView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <h1>Home Page</h1>
+    <p>Lorem ipsum dolor sit  amet consectetur, adipisicing elit. Nisi hic ullam unde pariatur corrupti commodi, mollitia voluptates exercitationem rem quam. Soluta ab nobis esse. Consequatur amet dicta voluptatem ex nihil repudiandae, rem cumque non, ipsa modi, libero aliquam praesentium adipisci? Eveniet tenetur sit aperiam quidem ex, doloremque deserunt eum amet! Quasi animi, quod aliquam eius eos inventore optio, voluptates sequi eveniet culpa, quaerat corrupti non a repellendus? Doloremque quibusdam magnam deserunt blanditiis? Error, ratione? Ut molestiae ipsam recusandae, doloribus earum ea ullam, natus eveniet eos saepe sint aperiam. Suscipit eius amet voluptas commodi doloribus nam. Officiis tenetur ipsa ducimus perferendis.</p>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nisi hic ullam unde pariatur corrupti commodi, mollitia voluptates exercitationem rem quam. Soluta ab nobis esse. Consequatur amet dicta voluptatem ex nihil repudiandae, rem cumque non, ipsa modi, libero aliquam praesentium adipisci? Eveniet tenetur sit aperiam quidem ex, doloremque deserunt eum amet! Quasi animi, quod aliquam eius eos inventore optio, voluptates sequi eveniet culpa, quaerat corrupti non a repellendus? Doloremque quibusdam magnam deserunt blanditiis? Error, ratione? Ut molestiae ipsam recusandae, doloribus earum ea ullam, natus eveniet eos saepe sint aperiam. Suscipit eius amet voluptas commodi doloribus nam. Officiis tenetur ipsa ducimus perferendis.</p>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'HomeView',
+  components: {
+   
+  }
+}
+</script>
